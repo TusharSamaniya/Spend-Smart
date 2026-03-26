7b06d5d8d68482ac33b8774293756b31ce9ce112
TusharSamaniya <tushasamaniya4956@gmail.com>
2026-03-26 16:43:50 +0530

    Generate the gst-service Spring Boot project

---
+++ services/gst-service/src/main/java/in/spendsmart/gstservice/GstServiceApplication.java
@@ -0,0 +1,13 @@
+package in.spendsmart.gstservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstServiceApplication.class, args);
+	}
+
+}
---
+++ services/gst-service/src/test/java/in/spendsmart/gstservice/GstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spendsmart.gstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
