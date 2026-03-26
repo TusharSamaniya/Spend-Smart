3283786194438d706f0b861f582d61f25c027c9b
TusharSamaniya <tushasamaniya4956@gmail.com>
2026-03-26 12:05:05 +0530

    Generate the upi-service Spring Boot project

---
+++ services/upi-service/src/main/java/in/spendsmart/upiservice/UpiServiceApplication.java
@@ -0,0 +1,13 @@
+package in.spendsmart.upiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiServiceApplication.class, args);
+	}
+
+}
---
+++ services/upi-service/src/test/java/in/spendsmart/upiservice/UpiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spendsmart.upiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
