64c776026217b3184b8c01a621b781f55e46a431
TusharSamaniya <tushasamaniya4956@gmail.com>
2026-03-28 02:22:13 +0530

    Generate the notification-service Spring Boot project

---
+++ services/notification-service/src/test/java/in/spendsmart/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spendsmart.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
