f790fd017a27196a90b091f334480bb64a446719
TusharSamaniya <tushasamaniya4956@gmail.com>
2026-03-28 11:30:25 +0530

    Start gateway and verify all routes work

--- services/expense-service/src/main/java/in/spendsmart/expense/controller/ExpenseController.java
+++ services/expense-service/src/main/java/in/spendsmart/expense/controller/ExpenseController.java
@@ -7,6 +7,7 @@ import in.spendsmart.expense.service.ExpenseService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/expenses")
 @RequiredArgsConstructor
+@Slf4j
 @Tag(name = "Expenses")
 public class ExpenseController {
 
@@ -38,8 +41,12 @@ public class ExpenseController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<ExpenseResponse>> list(@AuthenticationPrincipal CurrentUser currentUser) {
+    @GetMapping({"", "/"})
+    public ResponseEntity<List<ExpenseResponse>> list(
+            @AuthenticationPrincipal CurrentUser currentUser,
+            @RequestHeader(value = "X-Org-Id", required = false) String forwardedOrgId
+    ) {
+        log.info("Forwarded X-Org-Id header={}", forwardedOrgId);
         List<ExpenseResponse> response = expenseService.list(currentUser.getOrgId(), currentUser.getUserId());
         return ResponseEntity.ok(response);
     }
